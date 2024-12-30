a7793f4d1d560578f003fefddaba19a318a0dfaf
Asharu <asharumusk@gmail.com>
2024-12-30 14:41:00 +0530

    added the deployed app link

--- backend.py
+++ backend.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Configure CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000"],
+    allow_origins=["https://startypespredictor.streamlit.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
